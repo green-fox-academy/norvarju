613eb4f9af89666a9e54a5148f276618d9bc6507
Nóra Varju <snailjokes@gmail.com>
2018-02-15 15:23:11 +0100

    Upload Excercise_08 and 10

---
+++ week-01/day-4/src/Excercise_08.java
@@ -0,0 +1,15 @@
+public class Excercise_08 {
+    public static void main(String[] args) {
+        // Swap the values of the variables
+        int a = 123;
+        int b = 526;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        int t = a; a = b; b = t;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
