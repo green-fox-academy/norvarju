a9cd32f3b989f71276e8303e9a763f9058b7d367
Nóra Varju <snailjokes@gmail.com>
2018-02-15 12:22:23 +0100

    Upload .gitignore and Excercise_01

---
+++ week-01/day-4/src/Excercise_01.java
@@ -0,0 +1,6 @@
+public class Excercise_01 {
+    public static void main(String[] args) {
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello, norvarju!");
+    }
+}
