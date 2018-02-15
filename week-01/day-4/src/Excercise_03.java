4c767611e5fc8e8689a159673f53316e01178ed4
Nóra Varju <snailjokes@gmail.com>
2018-02-15 12:34:34 +0100

    Upload Excercise_03

---
+++ week-01/day-4/src/Excercise_03.java
@@ -0,0 +1,13 @@
+public class Excercise_03 {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hello, Samu!");
+        System.out.println("Hello, Peti!");
+        System.out.println("Hello, Laci!");
+    }
+}
