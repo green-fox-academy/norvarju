d5a5a84db56ed1593645048911adc67bb722dfe3
Nóra Varju <snailjokes@gmail.com>
2018-02-16 08:57:57 +0100

    Upload Excercise_23

---
+++ week-01/day-4/src/Excercise_23.java
@@ -0,0 +1,6 @@
+public class Excercise_23 {
+    public static void main(String[] args) {
+        for (int a = 0; a < 100; a++ )
+            System.out.println("I won't cheat on the exam!");
+    }
+}
