35de07b12201bef8850d5a3d80e8d2d45fa9c7d8
Nóra Varju <snailjokes@gmail.com>
2018-02-16 10:29:22 +0100

    Upload Excercise_23, 24, 25

--- week-01/day-4/src/Excercise_23.java
+++ week-01/day-4/src/Excercise_23.java
@@ -1,5 +1,7 @@
 public class Excercise_23 {
     public static void main(String[] args) {
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
         for (int a = 0; a < 100; a++ )
             System.out.println("I won't cheat on the exam!");
     }
---
+++ week-01/day-5/src/Excercise_24.java
@@ -0,0 +1,7 @@
+public class Excercise_24 {
+    public static void main(String[] args) {
+        // Create a program that prints all the even numbers between 0 and 500
+        for (int a = 0; a < 501; a += 2 )
+            System.out.println(a);
+    }
+}
