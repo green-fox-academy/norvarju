14c5ac3e32c85168d17b2d31579628fc1df80669
Nóra Varju <snailjokes@gmail.com>
2018-02-19 12:49:16 +0100

    Upload Excercise_03

---
+++ week-02/day-1/src/Excercise_03.java
@@ -0,0 +1,16 @@
+public class Excercise_03 {
+    public static void main (String[] args){
+        //  Create the usual class wrapper
+        //  and main method on your own.
+        // - Create a string variable named `am` and assign the value `kuty` to it
+        // - Write a function called `appendA` that gets a string as an input,
+        //   appends an 'a' character to its end and returns with a string
+        //
+        // - Print the result of `appendAFunc(am)`
+        String am = "kuty";
+        appendAFunc(am);
+    }
+    public static void appendAFunc (String fragment){
+        System.out.println(fragment + "a");
+    }
+}
