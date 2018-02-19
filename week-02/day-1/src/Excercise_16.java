b6f1c1674c296c6183edc41e99c61dcf9a8b7136
Nóra Varju <snailjokes@gmail.com>
2018-02-19 17:31:08 +0100

    Upload Excercise_16

---
+++ week-02/day-1/src/Excercise_16.java
@@ -0,0 +1,12 @@
+public class Excercise_16    {
+    public static void main(String[] args){
+        // - Create an array variable named `nimals`
+        //   with the following content: `["kuty", "macsk", "cic"]`
+        // - Add all elements an `"a"` at the end
+        String[] nimals = {"kuty", "macsk", "cic"};
+        for (int i = 0; i < nimals.length ; i++) {
+                System.out.println(nimals[i] + "a");
+            }
+        }
+
+    }
