ed06cba889ba15adc7261a1cb2aa26ad72988157
Nóra Varju <snailjokes@gmail.com>
2018-02-19 12:37:15 +0100

    Upload Excercise_01

---
+++ week-02/day-1/src/Excercise_01.java
@@ -0,0 +1,13 @@
+public class Excercise_01 {
+    public static void main(String[] args) {
+// - Create an integer variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Print the result of `doubling(ak)`
+        int ak = 123;
+        doublingFunction(ak);
+    }
+
+        public static void doublingFunction(int result) {
+            System.out.println(result*2);
+    }
+}
