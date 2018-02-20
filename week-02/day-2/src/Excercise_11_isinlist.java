a15849aa9eb5bc86d26719e368c3b618b29419a6
Nóra Varju <snailjokes@gmail.com>
2018-02-20 21:07:28 +0100

    Upload Excercise_11

---
+++ week-02/day-2/src/Excercise_11_isinlist.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Excercise_11_isinlist {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(2,4,6,8,10,12,14,16));
+        checkNums(list);
+        // Check if list contains all of the following elements: 4,8,12,16
+        // Create a method that accepts list as an input
+        // it should return "true" if it contains all, otherwise "false"
+    }
+    public static ArrayList checkNums(ArrayList<Integer> numberList){
+        List<Integer> randomNumbers = Arrays.asList(4,8,12,16);
+        if(numberList.containsAll((randomNumbers))){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+        return numberList;
+    }
+}
