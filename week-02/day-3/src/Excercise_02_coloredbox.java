39db8336b9a56b76e7ff534720ef569f1f4b5460
Nóra Varju <snailjokes@gmail.com>
2018-02-21 10:53:43 +0100

    Upload Excercise_01 and 02

---
+++ week-02/day-3/src/Excercise_02_coloredbox.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+
+        import java.awt.*;
+
+        import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Excercise_02_coloredbox {
+    public static void mainDraw(Graphics graphics) {
+        // Draw a box that has different colored lines on each edge.
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+            graphics.setColor(Color.BLUE);
+            graphics.drawLine(100, 100, 200, 100);
+            graphics.setColor(Color.GREEN);
+            graphics.drawLine(200, 100, 200, 200);
+            graphics.setColor(Color.RED);
+            graphics.drawLine(200, 200, 100, 200);
+            graphics.setColor(Color.YELLOW);
+            graphics.drawLine(100, 200, 100, 100);
+        }
+    }
+}
