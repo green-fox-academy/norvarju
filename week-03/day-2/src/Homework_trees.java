105215c91b727c9ae14f7584ae542370c2d3df3f
Nóra Varju <snailjokes@gmail.com>
2018-03-04 22:03:19 +0100

    feat(Homework):finish

---
+++ week-03/day-2/src/Homework_trees.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Homework_trees {
+    public static void main(String[] args){
+
+        ArrayList<String> treeAttributes = new ArrayList<>();
+        treeAttributes.add("type");
+        treeAttributes.add("leaf");
+        treeAttributes.add("age");
+        treeAttributes.add("sex");
+
+        ArrayList<String> treeOne = new ArrayList<>();
+        treeOne.add("pine");
+        treeOne.add("green");
+        treeOne.add("6");
+        treeOne.add("female");
+
+        ArrayList<String> treeTwo = new ArrayList<>();
+        treeOne.add("oak");
+        treeOne.add("green");
+        treeOne.add("14");
+        treeOne.add("male");
+
+        ArrayList<String> treeThree = new ArrayList<>();
+        treeOne.add("maple");
+        treeOne.add("red");
+        treeOne.add("10");
+        treeOne.add("male");
+
+        ArrayList<String> treeFour = new ArrayList<>();
+        treeOne.add("apple");
+        treeOne.add("green");
+        treeOne.add("8");
+        treeOne.add("female");
+
+        ArrayList<String> treeFive = new ArrayList<>();
+        treeOne.add("ash");
+        treeOne.add("green");
+        treeOne.add("6");
+        treeOne.add("transgender");
+    }
+}
