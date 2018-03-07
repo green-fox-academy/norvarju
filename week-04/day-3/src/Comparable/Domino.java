4cbe108a59c47f2174f08e1150e6e8d90352bdaf
Nóra Varju <snailjokes@gmail.com>
2018-03-07 14:32:06 +0100

    feat(Comparable_Domino): finish

---
+++ week-04/day-3/src/Comparable/Domino.java
@@ -0,0 +1,31 @@
+package Comparable;
+
+import java.util.Arrays;
+
+
+public class Domino implements Comparable<Domino>{
+    private final int[] values;
+
+    public Domino(int valueA, int valueB) {
+        this.values = new int[]{valueA, valueB};
+    }
+
+    public int[] getValues() {
+        return values;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + values[0] + ", " + values[1] + "]";
+    }
+
+    @Override
+    public int compareTo(Domino o) {
+        if(values[0] == o.values[0]){
+            return 0;}
+            else if(values[0] >= o.values[0]){
+            return 1;}
+        else {
+            return -1;}
+    }
+}
