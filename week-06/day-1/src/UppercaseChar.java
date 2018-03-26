f5918c5214cbea3aea066cb7769a4c574dfb5fa7
Nóra Varju <snailjokes@gmail.com>
2018-03-26 11:51:43 +0200

    feat(UppercaseChar): finish

---
+++ week-06/day-1/src/UppercaseChar.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UppercaseChar {
+  public static void main(String[] args) {
+    //Write a Stream Expression to find the uppercase characters in a string!
+    String myString = "hElLogOoDBYe";
+    List<Character> myStringChars = new ArrayList<>();
+    for (int i = 0; i < myString.length(); i++) {
+      myStringChars.add(myString.charAt(i));
+    }
+   List<Character> uppercaseChars = myStringChars.stream()
+            .filter((c) -> Character.isUpperCase((c)))
+            .collect(Collectors.toList());
+
+    System.out.println(uppercaseChars);
+  }
+}
