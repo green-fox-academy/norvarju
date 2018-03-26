f61c026e4de3f9a266bf44dc0d40d1aa7f5a7743
Nóra Varju <snailjokes@gmail.com>
2018-03-26 11:58:18 +0200

    feat(FromAToIStrings): finish

---
+++ week-06/day-1/src/fromAToIStrings6.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FromAToIStrings6 {
+  public static void main(String[] args) {
+   //Write a Stream Expression to find the strings which starts with A and ends with I in the
+    //following array:
+    ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+    List<String> filteredCities = cities.stream()
+            .filter(s -> s.startsWith("A") && s.endsWith("I"))
+            .collect(Collectors.toList());
+    System.out.println(filteredCities);
+  }
+}
