8fef342025dfb9cd9834428972a33632583695eb
Nóra Varju <snailjokes@gmail.com>
2018-03-26 13:53:06 +0200

    feat(ArrayToString): finish

---
+++ week-06/day-1/src/ArrayToString.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArrayToString {
+  public static void main(String[] args) {
+    //Write a Stream Expression to convert a char array to a string!
+    Character[] myChars = {'b', 'e', 'h', 'a', 'p', 'p', 'y'};
+    List<Character> listOfChars = new ArrayList<>(Arrays.asList(myChars));
+    String newWord = listOfChars.stream()
+            .map(c -> c.toString())
+            .collect(Collectors.joining(""))
+            .toString();
+
+    System.out.println(newWord);
+    //Older solution
+    String result = "";
+    for (int i = 0; i < myChars.length; i++) {
+      result += myChars[i];
+    }
+    System.out.println(result);
+
+
+  }
+}
