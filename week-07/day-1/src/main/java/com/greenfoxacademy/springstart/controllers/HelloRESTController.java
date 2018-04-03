f2a32dd6d365dae57eac5a45a83475a5e357d88e
Nóra Varju <snailjokes@gmail.com>
2018-04-03 14:09:23 +0200

    feat(Hello User REST APP upgrade): create

--- week-07/day-1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -2,16 +2,22 @@ package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class HelloRESTController {
 
-  @RequestMapping("/greeting")
+  /*@RequestMapping("/greeting")
   public Greeting greeting(){
     Greeting newGreeting = new Greeting(1, "Hello World");
     return newGreeting;
+  }*/
+
+  @RequestMapping("/greeting")
+  public Greeting greeting2(@RequestParam("name") String myName){
+  Greeting newGreeting = new Greeting(1, "Hello, " + myName + "!");
+    return newGreeting;
   }
-  
 }
