1ed8206d97c8bb4efb2efec27e3733c7215119d6
Nóra Varju <snailjokes@gmail.com>
2018-04-06 10:13:58 +0200

    feat(Fox Club Fox class): create

---
+++ week-07/day-4/src/main/java/com/greenfoxacademy/springstart/Model/Fox.java
@@ -0,0 +1,53 @@
+package com.greenfoxacademy.springstart.Model;
+
+import java.util.List;
+
+public class Fox {
+
+  private String name;
+  private List<String> listOfString;
+  private String food;
+  private String drink;
+
+  public Fox() {
+  }
+
+  public Fox(String name, List<String> listOfString, String food, String drink) {
+    this.name = name;
+    this.listOfString = listOfString;
+    this.food = food;
+    this.drink = drink;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public List<String> getListOfString() {
+    return listOfString;
+  }
+
+  public String getFood() {
+    return food;
+  }
+
+  public String getDrink() {
+    return drink;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setListOfString(List<String> listOfString) {
+    this.listOfString = listOfString;
+  }
+
+  public void setFood(String food) {
+    this.food = food;
+  }
+
+  public void setDrink(String drink) {
+    this.drink = drink;
+  }
+}
