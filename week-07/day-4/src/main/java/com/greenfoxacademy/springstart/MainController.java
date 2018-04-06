4d600f6d5201a650615c47dca9c2dd89fb748e9b
Nóra Varju <snailjokes@gmail.com>
2018-04-06 10:10:14 +0200

    feat(Fox Club main page): create

--- week-07/day-4/src/main/java/com/greenfoxacademy/springstart/MainController.java
+++ week-07/day-4/src/main/java/com/greenfoxacademy/springstart/MainController.java
@@ -1,4 +1,13 @@
 package com.greenfoxacademy.springstart;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+  @GetMapping("/")
+  public String login(){
+    return "login";
+  }
 }
