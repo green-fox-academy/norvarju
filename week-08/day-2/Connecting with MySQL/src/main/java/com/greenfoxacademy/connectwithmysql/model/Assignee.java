77dee69b1b511582648ea89d69775c56131a5c84
Nóra Varju <snailjokes@gmail.com>
2018-04-13 09:39:18 +0200

    feat(Todo BootStrap app mainpage link): create

--- week-08/day-2/Connecting with MySQL/src/main/java/com/greenfoxacademy/connectwithmysql/model/Assignee.java
+++ week-08/day-2/Connecting with MySQL/src/main/java/com/greenfoxacademy/connectwithmysql/model/Assignee.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Assignee {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
   private String name;
