d925700442e91db316c9b8c0b1dbd36baa60f04b
Nóra Varju <snailjokes@gmail.com>
2018-04-25 13:43:20 +0200

    feat(BookSearch App four view): create

---
+++ Huli/SearchBook/app/src/main/java/com/greenfoxacademy/searchbook/SeachByAuthor.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.searchbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeachByAuthor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seach_by_author);
+    }
+}
